10a871bef5aa8b69a2523f3eca42380c38b7fb34
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-13 08:37:02 +0200

    Add BankiSzolgaltatas.java

---
+++ src/BankiSzolgaltatas.java
@@ -0,0 +1,9 @@
+public class BankiSzolgaltatas {
+    private Tulajdonos tulajdonos;
+    public BankiSzolgaltatas(Tulajdonos tulajdonos){
+        this.tulajdonos = tulajdonos;
+    }
+    public Tulajdonos getTulajdonos(){
+        return tulajdonos;
+    }
+}
