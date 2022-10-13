6c130635b6f18e4f3020a3bd337d3ba45a7027e8
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-13 08:51:56 +0200

    Add Szamla.java

---
+++ src/Szamla.java
@@ -0,0 +1,22 @@
+public class Szamla extends BankiSzolgaltatas{
+    private int aktualisEgyenleg;
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+    }
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+    public void befizet(int osszeg){
+        this.aktualisEgyenleg =+ osszeg;
+    }
+    public boolean kivesz(int osszeg){
+        if (osszeg > this.aktualisEgyenleg){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    /*public Kartya ujKartya(String kartyaszam){
+
+    }*/
+}
