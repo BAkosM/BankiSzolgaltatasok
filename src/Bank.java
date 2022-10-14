565d95e58b2a803f4c12add7d07300be5f38685e
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-14 16:35:49 +0200

    add functions to Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -6,4 +6,42 @@ public class Bank {
     public Bank(){
         
     }
+    public Szamla szamlanyitas(Tulajdonos tulajdonos, int hitelKeret){
+        if (hitelKeret >0){
+            szamlaLista.add(new HitelSzamla(tulajdonos, hitelKeret));
+            return new HitelSzamla(tulajdonos, hitelKeret);
+        } else if(hitelKeret == 0){
+            szamlaLista.add(new MegtakaritasiSzamla(tulajdonos));
+            return new MegtakaritasiSzamla(tulajdonos);
+        }else{
+            throw new IllegalArgumentException("A hitelkeret nem lehet kisebb mint 0");
+        }
+    }
+    public int getOsszEgyenleg(Tulajdonos tulajdonos){
+        int osszeegyenleg = 0;
+        for (Szamla szamla : szamlaLista) {
+            if (szamla.getTulajdonos() == tulajdonos) {
+                osszeegyenleg += szamla.getAktualisEgyenleg();
+            }
+        }
+        return osszeegyenleg;
+    }
+    public int getLegnagyobbEgyenleguSzamla(Tulajdonos tulajdonos){
+        int max = 0;
+        for (Szamla szamla : szamlaLista) {
+            if (szamla.getTulajdonos() == tulajdonos && szamla.getAktualisEgyenleg() > max) {
+                max = szamla.getAktualisEgyenleg();
+            }
+        }
+        return max;
+    }
+    public long getOsszhitelkeret(){
+        int osszeg = 0;
+        for (Szamla szamla : szamlaLista){
+            if (szamla instanceof HitelSzamla){
+                osszeg+=((HitelSzamla) szamla).getHitelKeret();
+            }
+        }
+        return osszeg;
+    }
 }
